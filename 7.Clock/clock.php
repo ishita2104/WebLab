32269cebc948922c91baaeeb0360efed059364e5
Ishita Chattopadhyay <60072783+ishita2104@users.noreply.github.com>
2021-08-14 01:20:01 +0530

    Update clock.php

--- 7.Clock/clock.php
+++ 7.Clock/clock.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DIGITAL CLOCK</title>
+	<meta http-equiv="refresh" content="1"> <!--refersh after every 1sec -->
+	<style>
+		body{
+			font-size: 30px;
+			color: white;
+			background-color: black;
+			border:6px solid #36454F;
+			text-align:center;
+			height: 110px;
+			width: 200px;
+			padding: 25px 25px 25px 25px;
+			box-shadow: 5px 10px 8px #888888;
+			transform: translate(530px,100px);
+		}
+	</style>
+</head>
+<body>
+
+	<?php 
+		date_default_timezone_set("Asia/Kolkata");
+		echo "<center><br>".date("H : i : s a")."</br></center>";
+	?>
+
+</body>
+</html>
 
