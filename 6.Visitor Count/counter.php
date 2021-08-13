c053c5a81aec61b376d7a873feba5bfad28fad9e
Ishita Chattopadhyay <60072783+ishita2104@users.noreply.github.com>
2021-08-14 01:19:07 +0530

    Update counter.php

--- 6.Visitor Count/counter.php
+++ 6.Visitor Count/counter.php
@@ -1,1 +1,12 @@
-
+<?php
+ $file = "count.txt";
+ $count = 0;
+ $count = strval(file_get_contents($file));
+ $count = (int)$count;
+ file_put_contents($file, $count+1);
+ ?>
+ <br><br>
+ <?php
+ echo "<center><h1>ISHITA CHATTOPADHYAY - 1CR18CS077</h1></center>";
+ echo "<center> <h2>Visitor Count: ".$count."</h2></center>";
+?>
